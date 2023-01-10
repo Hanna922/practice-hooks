5bf61cf7ddeb61b9433565a1dc9f0b85e8a2a69b
Hanna922 <hanna.urssu@gmail.com>
2023-01-10 23:23:12 +0900

    ~#2.2 useClick

---
+++ src/useClick.js
@@ -0,0 +1,33 @@
+import { useEffect, useRef, useState } from "react";
+
+const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    if (typeof onClick !== "function") {
+      return;
+    }
+    // useEffect가 mount 되었을 때, update 되었을 때 call (여기서는 dependency를 []로 설정하였으므로 update 시 호출X)
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      // useEffect를 return 받은 함수는 componentWillUnMount 때 호출됨
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    }; // => 이렇게 하는 이유는 component가 mount되지 않았을 때 eventListener가 배치되지 않게 하기 위함
+  }, []);
+  return element;
+};
+
+const App = () => {
+  const sayHello = () => console.log("say hello");
+  const title = useClick(sayHello);
+  return (
+    <div className="App">
+      <h1 ref={title}>Hi</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/useInput.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event; // ES6, const value = event.target.value 와 동일
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
+const App = () => {
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
+    </div>
+  );
+};
+
+export default App;
